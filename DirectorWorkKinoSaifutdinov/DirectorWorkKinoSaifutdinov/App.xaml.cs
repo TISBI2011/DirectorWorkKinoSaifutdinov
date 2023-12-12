271c09abb4bf8ee911af9687c469e9c7bcd2a06f
ИС201-1 <IS201-1@tisbi.ru>
2023-12-12 16:55:05 +0300

    push

---
+++ DirectorWorkKinoSaifutdinov/DirectorWorkKinoSaifutdinov/App.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using DirectorWorkKinoSaifutdinov.Models;
+
+namespace DirectorWorkKinoSaifutdinov
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public static DirectorWorkKinoSaifutdinovEntities DB = new DirectorWorkKinoSaifutdinovEntities();
+         
+        App.DB.Models.ToList() 
+    }
+}
